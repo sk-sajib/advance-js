ebbb3315746db36d031e3650788b8411e58c274f
sk-sajib <sksajib0000@gmail.com>
2020-06-16 21:26:54 +0600

    truthy-falsy value experiemnt done

---
+++ truthy-falthy.js
@@ -0,0 +1,79 @@
+
+// ` Truthy value `
+
+// ' ', '0', [], {}, any numbber without zero
+
+// Falsy Value
+
+// '',0,Undefiend,Null, NaN
+
+
+// Truthy valur Experiement
+
+//const age = ' '
+
+// if(age){
+//     console.log('I am true, buddy')
+// } else{
+//     console.log('I am false, buddy')
+// }
+
+
+// const name = '0'
+
+// if(name){
+//     console.log('I am true, buddy')
+// } else{
+//     console.log('I am false, buddy')
+// }
+
+// const name = []
+
+// if(name){
+//     console.log('I am true, buddy')
+// } else{
+//     console.log('I am false, buddy')
+// }
+
+
+// const name = {}
+// if(name){
+//     console.log('I am true, buddy')
+// } else{
+//     console.log('I am false, buddy')
+// }
+
+
+
+// Fasly Value Experiment
+
+
+// const name = ''
+// if(name){
+//     console.log('I am true, buddy')
+// } else{
+//     console.log('I am false, buddy')
+// }
+
+// const age = 0
+// if(age){
+//     console.log('I am true, buddy')
+// } else{
+//     console.log('I am false, buddy')
+// }
+
+
+
+const name = NaN
+if(name){
+    console.log('I am true, buddy')
+} else{
+    console.log('I am false, buddy')
+}
+
+const mori = null
+if(mori){
+    console.log('I am true, buddy')
+} else{
+    console.log('I am false, buddy')
+}
