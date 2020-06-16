71966501b11e59fe05211f6af5460b84b42b6318
sk-sajib <sksajib0000@gmail.com>
2020-06-17 00:45:51 +0600

    slice,splice,join

---
+++ slice,splice,join.js
@@ -0,0 +1,13 @@
+
+const arr = [2,3,6,7,8,9,0]
+const arr1= [12,13]
+
+// const part = arr.slice(1,6) // slice = array ar majkhan theke kete fele, kinto existing array ar kono poriborton kore na
+// console.log(part)
+
+// const removed = arr.splice(1,5, 27)
+// console.log(removed)        // splice =( start index, till delet index count, add in removed index  )
+// console.log(arr)
+
+const added = arr.join(' ')  // make array together
+console.log(added)
