606ee5553aba6f41bc36011bd4737d9943f0d7f7
sk-sajib <sksajib0000@gmail.com>
2020-06-16 22:33:22 +0600

    ==vs===

---
+++ ==vs===.js
@@ -0,0 +1,14 @@
+
+const a = 1
+const b = true
+
+
+if(a===b){
+    console.log('this is true')
+} else{
+   console.log('this is not true') 
+}
+
+
+// == SODO VALUE KE COMPARE KORE
+// === VALUE AND TYPE DOUITAR SATHE COMPARE KORE OUTPUT RESULT DIBE
